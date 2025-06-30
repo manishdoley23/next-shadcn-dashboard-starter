61a9007444f8193ccf20fc1ff6607e42edede8fe
manish23 <manishdoley23@gmail.com>
2025-06-30 21:16:38 +0530

    chore: first draft

--- src/app/dashboard/overview/layout.tsx
+++ src/app/dashboard/overview/layout.tsx
@@ -24,7 +24,8 @@ export default function OverViewLayout({
 }) {
   return (
     <PageContainer>
-      <div className='flex flex-1 flex-col space-y-2'>
+      <div>Hello</div>
+      {/* <div className='flex flex-1 flex-col space-y-2'>
         <div className='flex items-center justify-between space-y-2'>
           <h2 className='text-2xl font-bold tracking-tight'>
             Hi, Welcome back 👋
@@ -124,14 +125,11 @@ export default function OverViewLayout({
         </div>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
           <div className='col-span-4'>{bar_stats}</div>
-          <div className='col-span-4 md:col-span-3'>
-            {/* sales arallel routes */}
-            {sales}
-          </div>
+          <div className='col-span-4 md:col-span-3'>{sales}</div>
           <div className='col-span-4'>{area_stats}</div>
           <div className='col-span-4 md:col-span-3'>{pie_stats}</div>
         </div>
-      </div>
+      </div> */}
     </PageContainer>
   );
 }
